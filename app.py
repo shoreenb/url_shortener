5741a91a83ba90d1ba5cfd2ef14d4cdd71835597
Shoreen Beckford <shoreen.beckford@gmail.com>
2022-07-12 17:48:44 +0100

    add home.html template and add to def home()

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello World!"
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == "POST":
+        url_received = request.form["url_nm"]
+        return url_received
+    else:
+        return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
